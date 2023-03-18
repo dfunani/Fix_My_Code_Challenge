c166c50271f155b21d2803ae1f4f90805b90f6e2
Delali Funani <dfunani@live.co.za>
2023-03-18 05:45:05 -0700

    added

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -14,10 +14,11 @@ if (process.argv.length <= 2) {
     process.exit(1)
 }
 
-let size = Number.parseInt(process.argv[2])
+let size = Number.parseInt(process.argv[2], 16)
 
 if(!size)
 {
+    process.stderr.write("Usage: ./1-print_square.js <integer>\n");
     process.exit(1);
 }
 
